2e2757587f1f48df75b2f41a55b632f41578a961
vinmila <31014219+vinmila@users.noreply.github.com>
2017-08-21 22:23:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-1', function (req, res){
+    res.send('Article -1 is displayed');
+});
+
+app.get('/article-2', function (req, res){
+    res.send('Article -1 is displayed');
+});
+
+app.get('/article-3', function (req, res){
+    res.send('Article -1 is displayed');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

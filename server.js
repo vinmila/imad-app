ebb5fc1ca53ebae621babdc8cde5e8132f60e295
vinmila <31014219+vinmila@users.noreply.github.com>
2017-08-21 22:41:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-1', function (req, res){
-    res.send('Article -1 is displayed');
+    res.sendFile(path.join(__dirname, 'ui', 'article-1.html.html'));
 });
 
 app.get('/article-2', function (req, res){
